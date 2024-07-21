7f6e176cb5492e1d5e5e0146614eafd8a63f0145
Brendan <brendoniansl@gmail.com>
2024-07-21 09:40:46 -0400

    Index Base Structure + CSS + JS
    Created an index page with it's basic structure and functionality. CSS styles applied but do not include media queries since the screen scales fine to all screen sizes.

---
+++ public/js/menuButton.js
@@ -0,0 +1,28 @@
+// Grabs Menu Button From Document
+const menuButton = document.getElementById('menu');
+
+// Creates Menu Event
+menuButton.addEventListener('click', displayMenu);
+
+const exitButton = document.getElementById('exit');
+
+exitButton.addEventListener('click', displayMenu);
+
+// Grabs Menu Screen
+const menuScreen = document.getElementById('ms');
+//Grabs Menu Options Div
+const menuOptions = document.getElementById('ms-options');
+//Grabs Height Of 
+const menuHeaderHeight = document.getElementById('ms-header').offsetHeight;
+
+const headerHeight = document.querySelector('header').offsetHeight;
+const mainHeight = document.querySelector('main').offsetHeight;
+
+function displayMenu() {
+    if (menuScreen.style.display === "none" || menuScreen.style.display === "") {
+        menuScreen.style.display = "flex";
+        menuOptions.style.height = `calc(100vh - ${menuHeaderHeight}px)`;
+    } else {
+        menuScreen.style.display = "none";
+    }
+}
